bf1c3a227aead09db6ee20bd2574e9060be34712
Eduardo Rosas Osorno <lalorosas@outlook.com>
2016-04-17 11:45:20 -0500

    Add project files.

---
+++ MortgageCalculator_Android/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using MortgageCalculator_PCL;
+
+namespace MortgageCalculator_Android
+{
+    [Activity(Label = "MortgageCalculator_Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        EditText mortgageAmountEditText, interestRateEditText, mortgageTermEditText;
+        TextView monthlyPaymentTextView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.calculateButton);
+            mortgageAmountEditText = FindViewById<EditText>(Resource.Id.mortgageAmountEditText);
+            interestRateEditText = FindViewById<EditText>(Resource.Id.interestRateEditText);
+            mortgageTermEditText = FindViewById<EditText>(Resource.Id.mortgageTermEditText);
+            monthlyPaymentTextView = FindViewById<TextView>(Resource.Id.monthlyPaymentTextView);
+
+            button.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            float mortgageAmount = float.Parse(mortgageAmountEditText.Text);
+            float interestRate = float.Parse(interestRateEditText.Text);
+            int term = int.Parse(mortgageTermEditText.Text);
+
+            monthlyPaymentTextView.Text = $"{Calculator.CalculateMonthlyPayment(mortgageAmount, interestRate, term):C}";
+        }
+    }
+}
+
---
+++ MortgageCalculator_PCL/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortgageCalculator_PCL
+{
+    public class Calculator
+    {
+        public static float CalculateMonthlyPayment(float dollarsMortgageAmount, float percentageInterestRate, int yearsMortgageTerm)
+        {
+            float monthlyPayment = 0.0f;
+
+            float monthlyInterestRate = (percentageInterestRate / 100) / 12;
+            float monthsMortgageTerm = yearsMortgageTerm * 12;
+
+            monthlyPayment = (monthlyInterestRate * dollarsMortgageAmount * ((float)Math.Pow((1 + monthlyInterestRate), monthsMortgageTerm))) / (((float)Math.Pow((1 + monthlyInterestRate), monthsMortgageTerm)) - 1);
+
+            return monthlyPayment;
+        }
+    }
+}
